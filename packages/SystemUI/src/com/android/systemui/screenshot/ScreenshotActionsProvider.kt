73b7b42d7dcd0bb4e04b5753dafc06d873070340
Dhina17 <dhinalogu@gmail.com>
2024-11-09 13:40:00 +0530

    SystemUI: Remove screenshot action chip labels
    The icons are descriptive enough to convey the action without labels.
    This also matches the 16 QPR3 behavior on Pixels.
    
    Change-Id: I3a9e33c036f3f6ada98f036e06db8a55c3093082

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotActionsProvider.kt
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotActionsProvider.kt
@@ -103,7 +103,7 @@ constructor(
         actionsCallback.provideActionButton(
             ActionButtonAppearance(
                 AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_share),
-                context.resources.getString(R.string.screenshot_share_label),
+                null,
                 context.resources.getString(R.string.screenshot_share_description),
             ),
             showDuringEntrance = true,
@@ -128,7 +128,7 @@ constructor(
         actionsCallback.provideActionButton(
             ActionButtonAppearance(
                 AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_edit),
-                context.resources.getString(R.string.screenshot_edit_label),
+                null,
                 context.resources.getString(R.string.screenshot_edit_description),
             ),
             showDuringEntrance = true,
@@ -147,7 +147,7 @@ constructor(
         actionsCallback.provideActionButton(
             ActionButtonAppearance(
                 AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_delete),
-                context.resources.getString(R.string.screenshot_delete_label),
+                null,
                 context.resources.getString(R.string.screenshot_delete_description),
             ),
             showDuringEntrance = true,
@@ -185,7 +185,7 @@ constructor(
             actionsCallback.provideActionButton(
                 ActionButtonAppearance(
                     AppCompatResources.getDrawable(context, R.drawable.ic_screenshot_scroll),
-                    context.resources.getString(R.string.screenshot_scroll_label),
+                    null,
                     context.resources.getString(R.string.screenshot_scroll_label),
                 ),
                 showDuringEntrance = true,
